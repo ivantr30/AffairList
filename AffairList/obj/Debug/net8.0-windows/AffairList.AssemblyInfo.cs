978ba4bad23c59c8cb106a3dd2169977900e4e9e
ivanchikinator <konfetnik2014@gmail.com>
2025-05-05 18:52:04 +0300

    1.1.6.1 GITIGNOREUPDATE In this update gitignore file now does not contain files made by program during work

--- AffairList/obj/Debug/net8.0-windows/AffairList.AssemblyInfo.cs
+++ AffairList/obj/Debug/net8.0-windows/AffairList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AffairList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d591bc63ed998c7b4413008392af5b119d43f11b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f55c9f0979b54f117eb57ecb0e97af096233642")]
 [assembly: System.Reflection.AssemblyProductAttribute("AffairList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AffairList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
