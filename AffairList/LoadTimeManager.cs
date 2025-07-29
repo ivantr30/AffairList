9ba7fabc6dd0523065c0d3ccd3389a75a69cb27a
ivanchikinator <konfetnik2014@gmail.com>
2025-07-29 12:28:27 +0300

    1.2.3

--- AffairList/LoadTimeManager.cs
+++ AffairList/LoadTimeManager.cs
@@ -13,45 +13,54 @@ namespace AffairList
             LoadTimeFileFullPath = Application.StartupPath + "loadtime.json";
             _loadTime = new LoadTimeModel();
 
-            Initialize(settings);
+            Task.Run(() => Initialize(settings));
         }
-        public void Initialize(Settings settings)
+        public async Task Initialize(Settings settings)
         {
             if (!LoadTimeFileExist()) CreateLoadTimeFile();
             try
             {
+                var readingLoadTimeFileResult = File.ReadAllTextAsync(LoadTimeFileFullPath);
+                await readingLoadTimeFileResult;
                 _loadTime = JsonConvert
-                    .DeserializeObject<LoadTimeModel>(File.ReadAllText(LoadTimeFileFullPath))!;
+                    .DeserializeObject<LoadTimeModel>(readingLoadTimeFileResult.Result)!;
                 if (_loadTime == null) throw new Exception("loadfile is null");
-                if (GetPreviousLoadTime().Day != DateTime.Now.Day && settings.DoesNotificate())
+                if ((GetPreviousLoadTime().Day != DateTime.Now.Day ||
+                    DateTime.Now.Hour - GetPreviousLoadTime().Hour >= 8)
+                    && settings.DoesNotificate())
                 {
-                    string[] affairs = File.ReadAllLines(settings.GetCurrentProfile());
-                    foreach (string affair in affairs)
+                    string[] profiles = Directory.GetFiles(settings.listsDirectoryFullPath);
+                    foreach (var profile in profiles)
                     {
-                        if (affair.StartsWith("<deadline>"))
+                        var readingFileResult = File.ReadAllLinesAsync(profile);
+                        await readingFileResult;
+                        string[] affairs = readingFileResult.Result;
+                        foreach (string affair in affairs)
                         {
-                            DateTime deadline = DateTime.Parse(affair.Substring(10, 11));
-                            int daysLeft = 0;
-                            daysLeft = deadline.Day - DateTime.Now.Day;
-                            if (daysLeft <= settings.GetNotificationDayDistance())
+                            if (affair.StartsWith("<deadline>"))
                             {
-                                NotifyIcon notification = new NotifyIcon();
-                                notification.Icon = SystemIcons.Exclamation;
-                                notification.BalloonTipTitle = "AffairList";
+                                DateTime deadline = DateTime.Parse(affair.Substring(10, 11));
+                                int daysLeft = 0;
+                                daysLeft = deadline.Day - DateTime.Now.Day;
+                                if (daysLeft <= settings.GetNotificationDayDistance())
+                                {
+                                    NotifyIcon notification = new NotifyIcon();
+                                    notification.Icon = SystemIcons.Exclamation;
+                                    notification.BalloonTipTitle = "AffairList";
 
-                                if(daysLeft < 1 && daysLeft >= 0)
-                                    notification.BalloonTipText = affair.Substring(10) + $" - осталось {24 - DateTime.Now.Hour} часов";
-                                else if(daysLeft == 1)
-                                    notification.BalloonTipText = affair.Substring(10) + " - остался 1 день";
-                                else if(daysLeft > 1)
-                                    notification.BalloonTipText = affair.Substring(10) + $" - осталось {daysLeft} дней";
-                                else
-                                    notification.BalloonTipText = affair.Substring(10) + " - просрочено";
+                                    if (daysLeft < 1 && daysLeft >= 0)
+                                        notification.BalloonTipText = affair.Substring(10) + $" - осталось {24 - DateTime.Now.Hour} часов";
+                                    else if (daysLeft == 1)
+                                        notification.BalloonTipText = affair.Substring(10) + " - остался 1 день";
+                                    else if (daysLeft > 1)
+                                        notification.BalloonTipText = affair.Substring(10) + $" - осталось {daysLeft} дней";
+                                    else
+                                        notification.BalloonTipText = affair.Substring(10) + " - просрочено";
 
-                                notification.Visible = true;
-                                notification.ShowBalloonTip(3000);
-                                notification.Dispose();
-
+                                    notification.Visible = true;
+                                    notification.ShowBalloonTip(1000);
+                                    notification.Dispose();
+                                }
                             }
                         }
                     }
@@ -60,7 +69,7 @@ namespace AffairList
             catch
             {
                 WriteBaseTime();
-                Application.Restart();
+                await Initialize(settings);
             }
         }
 
