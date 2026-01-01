1feac8ad9c1ecc74350b6929cf2fa9a5061eedcc
ivantr <konfetnik2014@gmail.com>
2026-01-01 10:20:30 +0300

    1.3.5.8 AffairListLogo AutoDownload now tray icon downloads automaticly if not exists

--- AffairList/Classes/TrayIconManager.cs
+++ AffairList/Classes/TrayIconManager.cs
@@ -4,6 +4,8 @@
     {
         public NotifyIcon TrayIcon { get; private set; }
         private ContextMenuStrip _trayMenu;
+        private const string LogoName = "AffairListLogo.ico";
+        private const string LogoUrl = "https://raw.githubusercontent.com/ivantr30/AffairList/refs/heads/master/AffairListLogo.ico";
 
         public TrayIconManager()
         {
@@ -12,10 +14,31 @@
 
             TrayIcon.BalloonTipTitle = "AffairList";
             TrayIcon.Text = "AffairList";
-            TrayIcon.Icon = Icon.ExtractAssociatedIcon("AffairListLogo.ico");
+            if(File.Exists(LogoName)) TrayIcon.Icon = Icon.ExtractAssociatedIcon(LogoName);
+            else Task.Run(DownloadIcon);
 
             TrayIcon.ContextMenuStrip = _trayMenu;
             TrayIcon.Visible = true;
+        }
+        private async Task DownloadIcon()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(LogoUrl);
+                    response.EnsureSuccessStatusCode();
+                    using (FileStream creatingStream = File.Create(LogoName))
+                    {
+                        response.Content.ReadAsStream().CopyTo(creatingStream);
+                    }
+                }
+                TrayIcon.Icon = Icon.ExtractAssociatedIcon(LogoName);
+            }
+            catch
+            {
+                MessageBox.Show("Cannot download icon for tray");
+            }
         }
         public void AddTrayMenuAction(string actionName, EventHandler action)
         {
